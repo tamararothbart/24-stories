80e56c5b7a53d8d54bd5b8b3acf68ff93eb48d54
Tamara Rothbart <tamararothbart@Tamaras-Air.lan>
2026-05-24 17:46:17 +0200

    Remove diagnostic logging from payfast-webhook

--- netlify/functions/payfast-webhook.js
+++ netlify/functions/payfast-webhook.js
@@ -3,10 +3,6 @@ exports.handler = async function(event) {
     return { statusCode: 405, body: 'Method not allowed' };
   }
 
-  // Log raw IPN for debugging
-  console.log('PAYFAST IPN RAW BODY:', event.body);
-  console.log('PAYFAST IPN HEADERS:', JSON.stringify(event.headers));
-
   // PayFast sends application/x-www-form-urlencoded
   const rawBody = event.isBase64Encoded
     ? Buffer.from(event.body || '', 'base64').toString('utf8')
